54832baf52f98292ddd3ddf1d9346c2c7454d2d7
Trần Ngọc Tiến <79617804+ngoctienTNT@users.noreply.github.com>
2021-10-29 08:10:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,84 @@ toggle.addEventListener("click", () => {
 
     }
 })
+
+//Chống copy
+function killCopy(e){
+    return false;
+}
+
+function reEnable(){
+    return true;
+}
+
+document.onselectstart=new Function ("return false");
+
+if (window.sidebar){
+    document.onmousedown=killCopy;
+    document.onclick=reEnable;
+}
+
+function noteOut()
+{
+    var note = document.querySelector(".note");
+    note.style.display = "none";
+}
+
+setInterval(noteOut,3000);
+
+//Chống chuột phải 
+window.onload = function() {
+    document.addEventListener("contextmenu", function(e) {
+        e.preventDefault();
+    }, false);
+    document.addEventListener("keydown", function(e) {
+        //document.onkeydown = function(e) {
+        // "I" key
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+            disabledEvent(e);
+        }
+        // "J" key
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+            disabledEvent(e);
+        }
+        // "S" key + macOS
+        if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+            disabledEvent(e);
+        }
+        // "U" key
+        if (e.ctrlKey && e.keyCode == 85) {
+            disabledEvent(e);
+        }
+        // "F12" key
+        if (event.keyCode == 123) {
+            disabledEvent(e);
+        }
+    }, false);
+
+    function disabledEvent(e) {
+        if (e.stopPropagation) {
+            e.stopPropagation();
+        } else if (window.event) {
+            window.event.cancelBubble = true;
+        }
+        e.preventDefault();
+        return false;
+    }
+};
+
+//Chống Ctrl + U
+document.onkeydown = function(e) {
+    if (e.ctrlKey && 
+        (e.keyCode === 67 || 
+         e.keyCode === 86 || 
+         e.keyCode === 85 || 
+         e.keyCode === 117)) {
+        return false;
+    } else {
+        return true;
+    }
+};
+$(document).keypress("u",function(e) {
+    if(e.ctrlKey) return false;
+    else return true;
+});
